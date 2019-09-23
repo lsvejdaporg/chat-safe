0c7971343f114c735ef3d529c52b9ca653bb1849
svejda <svejda@porg.cz>
2019-09-23 17:41:08 +0200

    /jinastranka

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ http.createServer((req, res) => {
     if (req.url == "/jinastranka") {
         res.writeHead(200, {"Content-type": "text/html"});
         res.end("<html lang='cs'><head><meta charset='UTF8'></head><body>blablabla</body></html>");
+    } else if (req.url == "/jsondemo") {
+        res.writeHead(200, {"Content-type": "application/json"});
+        let obj = {};
+        obj.jmeno = "Bob";
+        obj.prijmeni = "Bobíček";
+        obj.rokNarozeni = 2002;
+        res.end(JSON.stringify(obj));
+    } else if (req.url == "/jsoncitac") {
+        res.writeHead(200, {"Content-type": "application/json"});
+        let obj = {};
+        obj.pocetVolani = citac;
+        res.end(JSON.stringify(obj));
     } else {
         res.writeHead(200, {"Content-type": "text/html"});
         res.end("<html lang='cs'><head><meta charset='UTF8'></head><body>Počet volání: " +citac + "</body></html>");
