189387044babb00c5c51e1c753da9189c6e846eb
svejda <svejda@porg.cz>
2019-09-23 17:27:50 +0200

    /jinastranka

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ http.createServer((req, res) => {
     if (req.url == "/") {
         citac++; //dtto citac=citac+1
     }
-    res.writeHead(200, {"Content-type": "text/html"});
-    res.end("<html lang='cs'><head><meta charset='UTF8'></head><body>Počet volání: " +citac + "</body></html>");
+    if (req.url == "/jinastranka") {
+        res.writeHead(200, {"Content-type": "text/html"});
+        res.end("<html lang='cs'><head><meta charset='UTF8'></head><body>blablabla</body></html>");
+    } else {
+        res.writeHead(200, {"Content-type": "text/html"});
+        res.end("<html lang='cs'><head><meta charset='UTF8'></head><body>Počet volání: " +citac + "</body></html>");
+    }
 }).listen(8888);
