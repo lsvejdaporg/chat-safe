1257665a65ca7166885336991216abf69bc8810b
svejda <svejda@porg.cz>
2019-09-23 17:22:36 +0200

    Citac po jedne

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const http = require('http');
 
+let citac = 0;
+
 http.createServer((req, res) => {
+    if (req.url == "/") {
+        citac++; //dtto citac=citac+1
+    }
     res.writeHead(200, {"Content-type": "text/html"});
-    res.end("<html lang='cs'><head><meta charset='UTF8'></head><body>Čauvejs!</body></html>");
+    res.end("<html lang='cs'><head><meta charset='UTF8'></head><body>Počet volání: " +citac + "</body></html>");
 }).listen(8888);
